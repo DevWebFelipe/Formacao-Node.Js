297f0a0d10bed24d6c99d95ff0a78e6d47414178
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-18 20:39:24 -0300

    Módulo: Fundamentos do Node.Js

--- 01-Fundamentos-Node.Js/src/server.js
+++ 01-Fundamentos-Node.Js/src/server.js
@@ -1,26 +1,30 @@
-// const a = 5;
-// const b = 4;
-
-// console.log("Valor de A + B = ", (a + b))
-
-// Aplicações HTTP => APIS
-// http.
-
-// Padrões de improtação
-
-//const http = require('node:http')
-// CommonJS => require (exemplo acima, por padrão NODE só aceita esse)
-
 import http from 'node:http'
-// ESModules => import/export (é o mais comumente usado, para rodar esse, 
-// tem que adicionar uma tag no package.json ("type": "module", ))
 
-//                                req    , res
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+  //console.log(method, url)
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de usuário')
+  } 
+
+  if (method === 'PUT' && url === '/users') {
+    return response.end('Alteração completa de usuário')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return response.end('Inserido usuário')
+  }
+
+  if (method === 'PATCH' && url === '/users') {
+    return response.end('Alteração parcial de usuário')
+  }
+
+  if (method === 'DELETE' && url === '/users') {
+    return response.end('Exclusão de usuário')
+  }
+
   return response.end('Server rodando corretamente!')
 })
 
 server.listen(3333)
-
-// http localhost:3333
-// node --watch .\src\server.js
