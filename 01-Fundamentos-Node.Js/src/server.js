3bcf0b638f4ee68e3c20a6480e9476668890619f
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-21 14:33:06 -0300

    Módulo: Fundamentos do Node.Js

--- 01-Fundamentos-Node.Js/src/server.js
+++ 01-Fundamentos-Node.Js/src/server.js
@@ -2,8 +2,22 @@ import http from 'node:http'
 //    https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
+
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    request.body = null
+  }
+
+  console.log(request.body)
 
   if (method === 'GET' && url === '/users') {
     return response
@@ -12,11 +26,19 @@ const server = http.createServer((request, response) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = request.body
+
     users.push({
       id: 1,
-      nome: 'Felipe Teixeira',
-      email: 'felipe.flptxr@gmail.com'
+      name,
+      email,
     })
+
+     users.push({
+       id: 2,
+       nome: 'Felipe Teixeira',
+       email: 'felipe.flptxr@gmail.com'
+     })
 
     return response.writeHead(201).end()
   }
