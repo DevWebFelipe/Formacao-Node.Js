a70bf9113c9c2d4852b5ca93e08ae3e9fd1bdfb5
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-27 18:45:15 -0300

    Projeto 'Base', tive diversos problemas com ESLint, knex.... zerei o projeto e estou pesquisando para instalar aos poucos

--- 02-API-REST-NodeJs/src/database.ts
+++ 02-API-REST-NodeJs/src/database.ts
@@ -1,11 +1,15 @@
 import { knex as setupKnex } from "knex";
 
 export const config = {
-  client: "sqlite",
+  client: "sqlite3",
   connection: {
     filename: "./temp/app.db",
   },
   useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './migrations',
+}
 };
 
 export const knex = setupKnex(config);
--- 02-API-REST-NodeJs/src/server.ts
+++ 02-API-REST-NodeJs/src/server.ts
@@ -1,13 +1,13 @@
 import fastify from "fastify";
-// import { knex } from "./database";
+import { knex } from "./database";
 
 const app = fastify();
 
-// app.get("/hello", async () => {
-//   const tables = knex("sqlite_schema").select("*");
+app.get("/hello", async () => {
+  const tables = knex("sqlite_schema").select("*");
 
-//   return tables;
-// });
+  return tables;
+});
 
 app
   .listen({
