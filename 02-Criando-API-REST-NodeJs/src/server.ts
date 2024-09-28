d8bb685970b0e918281ddc29918228a5d867736d
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-27 23:32:28 -0300

    configurado variáveis de ambiente, usado biblioteca zod, concluido banco de dados

--- 02-Criando-API-REST-NodeJs/src/server.ts
+++ 02-Criando-API-REST-NodeJs/src/server.ts
@@ -1,20 +1,10 @@
 import fastify from "fastify";
-// import crypto from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  // const transaction = await knex("transactions")
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: "Transação de teste",
-  //     amount: 1000,
-  //   })
-  //   .returning("*");
-
-  // const transaction = await knex("transactions").select("*");
-
   const transaction = await knex("transactions")
     .where("amount", 500)
     .select("*");
@@ -24,7 +14,7 @@ app.get("/hello", async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running!");
