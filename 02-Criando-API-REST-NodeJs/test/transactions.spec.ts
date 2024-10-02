391c1f0df066804c3d59a1a66ad1c1b3039f122d
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-02 00:29:15 -0300

    Teste da listagem de transações

---
+++ 02-Criando-API-REST-NodeJs/test/transactions.spec.ts
@@ -0,0 +1,61 @@
+import { it, beforeAll, afterAll, describe, expect } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
+
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it.skip("should be able to create a new transaction", async () => {
+    const response = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "Freelancer",
+        amount: 8000,
+        type: "credit",
+      })
+      .expect(201);
+
+    console.log("_________________________________________");
+    console.log(response.headers);
+    console.log("_________________________________________");
+    console.log(response.get("Set-Cookie"));
+    console.log("_________________________________________");
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "Freelancer",
+        amount: 8000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    console.log("_________________________________________");
+    console.log(cookies);
+    console.log("_________________________________________");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", String(cookies))
+      .expect(200);
+
+    console.log(listTransactionsResponse.body);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "Freelancer",
+        amount: 8000,
+        type: "credit",
+      }),
+    ]);
+  });
+});
