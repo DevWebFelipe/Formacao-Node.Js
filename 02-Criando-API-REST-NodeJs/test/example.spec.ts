594ffe02f2893bc15362294f595566b62e0615af
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-01 21:01:48 -0300

    Configuração de teste automatizado

---
+++ 02-Criando-API-REST-NodeJs/test/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+
+test("o usuário consegue criar uma nova transação", () => {
+  // fazer a chamada HTTP para criar uma nova transação
+
+  const responseStatusCode = 201; // simulação do código retornado
+
+  // validar se o registro foi inserido no banco
+  expect(responseStatusCode).toEqual(201);
+});
