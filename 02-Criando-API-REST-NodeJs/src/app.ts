9d36d9327706063aa715981a61bdb8fcc508ad8c
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-01 22:58:20 -0300

    Módulo: Criando API Rest com Node.Js

---
+++ 02-Criando-API-REST-NodeJs/src/app.ts
@@ -0,0 +1,20 @@
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import { transactionRoutes } from "./routes/transactions";
+import { requestLog } from "./middlewares/request-log";
+
+export const app = fastify();
+
+// Para que o cookie gerencia as rotas, ele deve ser registrado antes das rotas em si
+app.register(cookie);
+
+// vai ser executado para todas as rotas de todos os plugins
+app.addHook("preHandler", requestLog);
+
+app.get("/hello", () => {
+  return "Hello world!";
+});
+
+app.register(transactionRoutes, {
+  prefix: "transactions",
+});
--- 02-Criando-API-REST-NodeJs/test/example.spec.ts
+++ 02-Criando-API-REST-NodeJs/test/example.spec.ts
@@ -1,10 +1,26 @@
 import { expect, test } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
 
-test("o usuário consegue criar uma nova transação", () => {
+test("o usuário consegue criar uma nova transação", async () => {
   // fazer a chamada HTTP para criar uma nova transação
 
-  const responseStatusCode = 201; // simulação do código retornado
+  const response = await request(app.server).post("/transactions").send({
+    title: "Freelancer",
+    amount: 8000,
+    type: "credit",
+  });
 
   // validar se o registro foi inserido no banco
-  expect(responseStatusCode).toEqual(201);
+  expect(response.statusCode).toEqual(201);
+
+  // outra forma de fazer, fazendo direto
+  // await request(app.server)
+  //   .post("/transactions")
+  //   .send({
+  //     title: "Freelancer",
+  //     amount: 8000,
+  //     type: "credit",
+  //   })
+  //   .expect(201);
 });
