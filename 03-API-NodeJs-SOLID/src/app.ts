2784bd58b8309674e2453ae9b6938457cf2cc377
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-05 22:40:28 -0300

    Configurado prisma para gerenciamento do banco de dados

--- 03-API-NodeJs-SOLID/src/app.ts
+++ 03-API-NodeJs-SOLID/src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    id: "1",
+    name: "Felipe",
+    email: "teste@teste.com",
+  },
+});
