cf6804360704f32932c7233b4c98d506042c4efe
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-08 11:29:41 -0300

    Iniciado separação de arquivos

--- 03-API-NodeJs-SOLID/src/app.ts
+++ 03-API-NodeJs-SOLID/src/app.ts
@@ -1,13 +1,6 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
-const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    id: "1",
-    name: "Felipe",
-    email: "teste@teste.com",
-  },
-});
+app.register(appRoutes);
