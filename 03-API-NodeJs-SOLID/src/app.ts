399942b3913873983e48bf84f9186f2487f8c162
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-10-10 19:43:52 -0300

    Tratamento de erros

--- 03-API-NodeJs-SOLID/src/app.ts
+++ 03-API-NodeJs-SOLID/src/app.ts
@@ -1,6 +1,26 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _request, replay) => {
+  if (error instanceof ZodError) {
+    return replay
+      .status(400)
+      .send({ message: "Valçidation error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: fazer log para uma ferramenta externa como DataLog/NewRelic/Sentry
+  }
+
+  return replay.status(500).send({
+    message: "Internal server error.",
+  });
+});
